792476a850e6154e8ed5e6661d2b88b47e82974e
NicoPauwels <nico_pauwels@hotmail.com>
2020-11-17 10:14:49 +0000

    Completed FizzBuzz Challenge.

---
+++ spec/calcSpec.js
@@ -0,0 +1,27 @@
+describe("My Fizzbuzz Function", function() {
+    beforeEach(function(){
+        fizzbuzz = new fizzBuzz();
+    });
+
+describe("Check Number", function() {
+    it("should exist", function(){
+        expect(fizzBuzz).toBeDefined();
+    });
+    it("should return FizzBuzz when called as fizzBuzz(15)", function(){
+        var result = fizzBuzz(15)
+        expect(result).toBe("FizzBuzz");
+    });
+    it("should return Fizz when called as fizzBuzz(9)", function(){
+        var result = fizzBuzz(9)
+        expect(result).toBe("Fizz");
+    });
+    it("should return Buzz when called as fizzBuzz(10)", function(){
+        var result = fizzBuzz(10)
+        expect(result).toBe("Buzz");
+    });
+    it("should return 2 when called as fizzBuzz(2) ", function(){
+        var result = fizzBuzz(2)
+        expect(result).toBe(2);
+    });
+    });
+    });
